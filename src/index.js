7c4e9b4503c490f4d76ae49cec223b06f2c0d413
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-05-26 10:38:37 +0100

    add parallax scrolling to background images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//deals with navbar animation
 const menuButton = document.querySelector("#menu-button");
 const menu = document.querySelector("#menu");
 
@@ -10,3 +11,27 @@ function showHideMenu(event) {
 }
 
 menuButton.addEventListener("click", showHideMenu);
+
+//scroll animation for background
+var controller = new ScrollMagic.Controller({ vertical: true });
+var tween = new TimelineMax().add([
+  TweenMax.fromTo(
+    ".parallax-background #back-building-img",
+    1,
+    { left: 100 },
+    { left: -150, ease: Linear.easeNone }
+  ),
+  TweenMax.fromTo(
+    ".parallax-background #mid-building-img",
+    1,
+    { left: 80 },
+    { left: -100, ease: Linear.easeNone }
+  ),
+]);
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#trigger",
+  duration: $(window).width(),
+})
+  .setTween(tween)
+  .addTo(controller);
